b2677c95a27123afa65219bef332f4da11a4e259
kdi0618 <k.kodai0618@gmail.com>
2022-11-28 13:31:42 +0900

    mod: add description

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -10,6 +10,7 @@
         <h2 class="text-2xl leading-7 font-semibold">
           Welcome to your Nuxt Application
         </h2>
+        <h3>Learning deploy for AWS S3 via CircleCI</h3>
         <p class="mt-3 text-gray-600">
           We recommend you take a look at the <a href="https://nuxtjs.org" target="_blank" class="button--doc text-green-500 hover:underline">Nuxt Documentation</a>, whether you are new or have previous experience with the framework.<br>
         </p>
